fa66542c62c8a9eb7691dc631c210dcffb14c741
sanutezu123 <hrishikesh.malakar94@gmail.com>
2023-08-02 01:02:30 +0530

    'v2'

--- src/geometry/vector.ts
+++ src/geometry/vector.ts
@@ -19,12 +19,21 @@ export class Vector {
   get values() {
     return this.vector;
   }
+
+  /**
+   * Copy
+   * @returns returns a new copy of the current vector
+   */
+  copy = () => {
+    return new Vector([this.values[0], this.values[1], this.values[2]]);
+  };
+
   /**
    * Adding two vectors u and v
    */
   add = (u: Vector) => {
     for (let i = 0; i < this.vector.length; i++) {
-      this.vector[i] = this.vector[i] + u.vector[i];
+      this.vector[i] = this.vector[i] + u.values[i];
     }
     return this.vector;
   };
@@ -34,16 +43,123 @@ export class Vector {
    */
   sub = (u: Vector) => {
     for (let i = 0; i < this.vector.length; i++) {
-      this.vector[i] = this.vector[i] - u.vector[i];
+      this.vector[i] = this.vector[i] - u.values[i];
     }
     return this.vector;
   };
 
   /**
-   * Copy
-   * @returns returns a new copy of the current vector
+   * Check if two vectors are equal
    */
-  copy = () => {
-    return new Vector([this.values[0], this.values[1], this.values[2]]);
+  isEqualTo = (u: Vector) => {
+    for (let i = 0; i < this.vector.length; i++) {
+      if (this.vector[i] != u.values[i]) {
+        return false;
+      }
+      return true;
+    }
+  };
+
+  /**
+   * Current vector is greater than given vector
+   */
+  isGreaterThan = (u: Vector) => {
+    for (let i = 0; i < this.vector.length; i++) {
+      if (this.vector[i] > u.values[i]) {
+        return true;
+      }
+      return false;
+    }
+  };
+  /**
+   * Current vector is lesser than given vector
+   */
+  isLesserThan = (u: Vector) => {
+    for (let i = 0; i < this.vector.length; i++) {
+      if (this.vector[i] < u.values[i]) {
+        return true;
+      }
+      return false;
+    }
+  };
+
+  /**
+   * Get dimension x, y, z values of vector
+   */
+  getDimension = (index: number) => {
+    if (index >= this.values.length) {
+      throw new Error("Array index out of bound \n");
+    }
+    return this.values[index];
+  };
+
+  /**
+   * modify already created vector
+   */
+  assign = (index: number, value: number) => {
+    if (index >= this.values.length) {
+      throw new Error("Array index out of bound \n");
+    }
+    this.vector[index] = value;
+  };
+
+  /**
+   * Dot product of current and given vector
+   */
+  dot = (u: Vector) => {
+    if (this.vector.length != u.values.length) {
+      throw new Error("Both vectors must be of equal dimension \n");
+    }
+    let result = 0;
+    for (let i = 0; i < this.vector.length; i++) {
+      result += this.vector[i] * u.values[i];
+    }
+    return result;
+  };
+
+  /**
+   * cross product 2D
+   */
+  cross2D = (u: Vector) => {
+    if (this.vector.length != u.values.length) {
+      throw new Error("Both vectors must be of equal dimension \n");
+    }
+    return this.vector[0] * u.values[1] - this.vector[1] * u.values[0];
+  };
+
+  /**
+   * cross product 2D
+   */
+  cross3D = (u: Vector) => {
+    if (this.vector.length != u.values.length) {
+      throw new Error("Both vectors must be of equal dimension \n");
+    }
+    return new Vector([
+      this.vector[1] * u.values[2] - this.vector[2] * u.values[1],
+      this.vector[2] * u.values[0] - this.vector[0] * u.values[2],
+      this.vector[0] * u.values[1] - this.vector[1] * u.values[0],
+    ]).values;
+  };
+
+  /**
+   * Magnitude of current vector
+   */
+  magnitude = () => {
+    let sumOfSquares = 0;
+    for (let i = 0; i < this.vector.length; i++) {
+      sumOfSquares += Math.pow(this.vector[i], 2);
+    }
+    return Math.sqrt(sumOfSquares);
+  };
+
+  /**
+   * Normalize - returns and unit vector
+   */
+  normalize = () => {
+    const magnitude = this.magnitude();
+    for (let i = 0; i < this.vector.length; i++) {
+      this.assign(i, this.vector[i] / magnitude);
+    }
+    return this.vector;
   };
 }
--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,23 @@ document.getElementById("vector1")!.innerHTML = vector1.values.toString();
 document.getElementById("vector2")!.innerHTML = vector2.values.toString();
 document.getElementById("vector3")!.innerHTML = vector3.toString();
 document.getElementById("vector4")!.innerHTML = vector4.toString();
+
+console.log(vector1.isEqualTo(vector2));
+const vector5 = new Vector([20, 30]);
+const vector6 = new Vector([20, 30]);
+console.log(vector5.isEqualTo(vector6));
+console.log(vector6.getDimension(1));
+vector6.assign(1, 100);
+
+const vector7 = new Vector([3, 0]);
+const vector8 = new Vector([3, 3]);
+console.log(vector8.dot(vector7));
+
+const vector9 = new Vector([1, 0, 0]);
+const vector10 = new Vector([0, 1, 0]);
+console.log(vector9.cross3D(vector10));
+console.log(vector8.magnitude());
+
+const vector11 = new Vector([10, 0, 0]);
+console.log(vector11.magnitude());
+console.log(vector11.normalize());
